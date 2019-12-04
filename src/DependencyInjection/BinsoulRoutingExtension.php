bcce374d8924814c66630666c756e5c9ac1874d6
Sebastian Mößler <s.moeszler@binsoul.de>
2019-12-04 08:32:25 +0100

    Add configurable table prefix

--- src/DependencyInjection/BinsoulRoutingExtension.php
+++ src/DependencyInjection/BinsoulRoutingExtension.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace BinSoul\Symfony\Bundle\Routing\DependencyInjection;
 
+use BinSoul\Symfony\Bundle\Routing\EventListener\TablePrefixListener;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -15,5 +16,15 @@ class BinsoulRoutingExtension extends Extension
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
+
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $prefix = trim($config['prefix'] ?? '');
+
+        $definition = $container->getDefinition(TablePrefixListener::class);
+        $definition->setArgument(0, $prefix);
+        if ($prefix === '') {
+            $definition->clearTags();
+        }
     }
 }
